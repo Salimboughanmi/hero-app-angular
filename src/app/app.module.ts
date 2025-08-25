a5484ffe8289933ff571e4dfe09852610e9cb070
salimboughanmi <boughanmi1salim@gmail.com>
2025-08-25 15:18:48 +0100

    create my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { FormsModule } from '@angular/forms';
+import { HeroFirstComponent } from './hero-first/hero-first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    HeroFirstComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
